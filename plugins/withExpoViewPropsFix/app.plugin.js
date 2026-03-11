84a71010dc2a9f38d2709afbd222a937f854f01d
Soft-Nation-dev <emmanuelsoft8105@gmail.com>
2026-03-11 09:29:35 +0100

    fix: update plugin to ESM syntax for Expo

--- plugins/withExpoViewPropsFix/app.plugin.js
+++ plugins/withExpoViewPropsFix/app.plugin.js
@@ -1,41 +1,47 @@
-import { withDangerousMod } from "@expo/config-plugins";
-import fs from "fs";
-import path from "path";
+// plugins/withExpoViewPropsFix/app.plugin.js
+const { withDangerousMod } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
-export default function withExpoViewPropsFix(config) {
+function withExpoViewPropsFix(config) {
   return withDangerousMod(config, [
     "android",
     async (config) => {
-      const filePath = path.join(
-        config.modRequest.projectRoot,
-        "node_modules",
-        "expo-modules-core",
-        "common",
-        "cpp",
-        "fabric",
-        "ExpoViewProps.cpp"
-      );
-
-      if (!fs.existsSync(filePath)) {
-        console.log("ExpoViewProps.cpp not found, skipping patch");
-        return config;
-      }
-
-      let contents = fs.readFileSync(filePath, "utf8");
-
-      if (contents.includes('std::format("{}%", dimension.value)')) {
-        contents = contents.replace(
-          'std::format("{}%", dimension.value)',
-          'std::to_string(dimension.value) + "%"'
+      try {
+        const filePath = path.join(
+          config.modRequest.projectRoot,
+          "node_modules",
+          "expo-modules-core",
+          "common",
+          "cpp",
+          "fabric",
+          "ExpoViewProps.cpp"
         );
 
-        fs.writeFileSync(filePath, contents);
-        console.log("Patched ExpoViewProps.cpp successfully");
-      } else {
-        console.log("Patch already applied or not needed");
+        if (!fs.existsSync(filePath)) {
+          console.log("[withExpoViewPropsFix] ExpoViewProps.cpp not found, skipping patch");
+          return config;
+        }
+
+        let contents = fs.readFileSync(filePath, "utf8");
+
+        if (contents.includes('std::format("{}%", dimension.value)')) {
+          contents = contents.replace(
+            'std::format("{}%", dimension.value)',
+            'std::to_string(dimension.value) + "%"'
+          );
+          fs.writeFileSync(filePath, contents);
+          console.log("[withExpoViewPropsFix] Patched ExpoViewProps.cpp successfully ✅");
+        } else {
+          console.log("[withExpoViewPropsFix] Patch already applied or not needed");
+        }
+      } catch (err) {
+        console.error("[withExpoViewPropsFix] Error while patching:", err);
       }
 
       return config;
     },
   ]);
 }
+
+module.exports = withExpoViewPropsFix;
